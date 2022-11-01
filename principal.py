06dd230a7ec901cd5673665b9d8070ed684f4459
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-11-01 19:13:22 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,10 +6,14 @@ st.write('Oi, estamos no aplicativo para calcular o IMC') #st.write escreve na t
 #o usuario ira fornecer peso e altura
 peso = st.number_input('Digite o seu peso:')
 altura = st.number_input('Digite a sua altura:')
-
-
-
+#calcula o IMC
 if st.button('Calcular'):
   imc = peso/(altura*altura)
-  st.write('O seu IMC vale',imc)
+  if imc < 18.5:
+    st.write('O seu IMC vale',imc,'e esta abaixo do peso')
+  if imc >=18.5 and imc < 25:
+    st.write('O seu IMC vale',imc,'e esta com peso normal')
+  if imc >= 25:
+    st.write('O seu IMC vale',imc,'e esta acima do peso')
+  
 
