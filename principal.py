b2f1d633edd1028844a36ab7632b832dc3e76b19
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-11-01 19:03:57 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -4,6 +4,6 @@ st.title('APP Aula IA-EngBio: IMC')
 st.write('Oi, estamos no aplicativo para calcular o IMC') #st.write escreve na tela
 
 #o usuario ira fornecer peso e altura
-peso = number_input('Digite o seu peso:')
+peso = st.number_input('Digite o seu peso:')
 st.write(peso)
 
